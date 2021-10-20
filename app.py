948eb69bf40b764f8a89e2c886c3ce05ef334064
zeus <alexsandro.oliveira@pm.me>
2021-10-19 23:21:25 -0300

    Website - Template

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='web/static',
+            template_folder='web/templates')
 
 @app.route('/')
 def home():
